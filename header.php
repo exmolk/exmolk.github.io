7c485745b276dab4c5db5f5633353832061024d4
Janaka Chathuranga <bjchathuranga@gmail.com>
2017-03-31 02:32:04 +0530

    Fix logo

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="logo" class="img-responsive"></a>
+            <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="logo" class="img-responsive"></a>
           </div>
 
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
