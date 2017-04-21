83f10f16a1ed0dbb776707a91dbfad5c6164a463
Padmal <blog.padmal@gmail.com>
2017-04-22 02:38:45 +0530

    Quizmo content

---
+++ competition-quizmo.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php 
+    include("head.php");
+  ?>
+  <body>
+  
+    <?php 
+      include("header.php");
+    ?>
+
+    <!-- About start here -->
+    <section class="about gallery gallery-about padding-120">
+      <div class="container">
+        <div class="section-header text-center">
+          <h2>QuizMO 2017</h2>
+        </div>
+
+		<!--Slider Item-->
+		<div class="sponsor-item">
+          <div class="swiper-container sponsor-slider-one">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img src="images/departments/mechanical/cadmo1.jpg" alt="sponsor image" style="width:100%">
+                  </a>
+                </div>       
+              </div>
+              <div class="swiper-slide">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img src="images/departments/mechanical/cadmo2.jpg" alt="sponsor image" style="width:100%">
+                  </a>
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img src="images/departments/mechanical/cadmo3.jpg" alt="sponsor image" style="width:100%">
+                  </a>
+                </div>    
+              </div>
+              <div class="swiper-slide">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img src="images/departments/mechanical/cadmo4.jpg" alt="sponsor image" style="width:100%">
+                  </a>
+                </div>    
+              </div>
+              <div class="swiper-slide">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img src="images/departments/mechanical/cadmo5.jpg" alt="sponsor image" style="width:100%">
+                  </a>
+                </div>    
+              </div>
+              <div class="swiper-slide">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img src="images/departments/mechanical/cadmo6.png" alt="sponsor image" style="width:100%">
+                  </a>
+                </div>    
+              </div>
+              <div class="swiper-slide">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img src="images/departments/mechanical/cadmo7.jpg" alt="sponsor image" style="width:100%">
+                  </a>
+                </div>    
+              </div>
+            </div>
+          </div>
+        </div><!-- Slider-item -->
+	
+		<!--Content-->
+		<div class="long-content">
+
+			<h4>OVERVIEW </h4>
+
+			<div class="row">
+			    <div class="col-md-4 col-sm-4 col-xs-12">
+				    <div class="thumbnail">
+					    <a href="#">
+					    	<img src="images/departments/mechanical/cadmo_logo.png" alt="sponsor image" >
+			    		</a>
+			    	</div>  
+			    </div> 
+			</div>  
+
+			<p>
+			TLM QuizMO 2017 is a general knowledge quiz organized by the University of Moratuwa Department of Trasnpost and 
+			Logistics Management which will be held on 28th and 29th of April from 9.00AM onwards at the Department of 
+			Transport and Logistics Management. QuizMO 2017 is being held for the first time in this year associated with 
+			the upcoming science and technology exhibition “EXMO 2017”. The competition is open for the school students.
+			</p>
+
+			<h4>AIM</h4>
+
+			<p>
+			Value the general knowledge of the students rather just improving the academic knowledge 
+			</p>
+
+			<h4>PARTICIPANTS </h4>
+
+			<p>
+			The competition is open for the school students in Sri Lanka as individuals. The participants don’t have to 
+			register for the quiz prior to the competition. They will be selected on first come, first serve basis.
+			</p>
+
+			<h4>SCHEDULE</h4>
+
+			<p>
+			One quiz will be held per hour in the computer lab of the department 2nd floor. One quiz with 10 
+			questions will be held in each hour for 40 students and 10 quiz competitions will be held per each day. 
+			</p>
+
+			<h4>PRIZES</h4>
+
+			<p>
+			Results will be announced at the end of each competition. The winner of each quiz will be awarded 
+			with a valuable gift.
+			</p>
+
+			<h4>CONTACT DETAILS</h4>
+
+			<ul>
+				<li>Sachini Kumarasinghe		+94716379345 </li>
+				<li>Rahal Indrachapa		+94716602250</li>
+			</ul>
+
+		</div><!--Content-->        
+      
+      </div><!-- container -->
+	</section>
+    <!-- About end here -->
+
+    <?php 
+     include("footer.php");
+    ?>
+
+  </body>
+</html>
