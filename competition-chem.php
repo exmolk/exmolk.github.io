a0b0d509583898164c6f97ddda83f9f703b3332f
Padmal <blog.padmal@gmail.com>
2017-04-22 08:33:08 +0530

    Chem quiz

---
+++ competition-chem.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php 
+    include("head.php");
+  ?>
+  <body>
+  
+    <?php 
+      include("header.php");
+    ?>
+
+    <!-- About start here -->
+    <section class="about gallery gallery-about padding-120">
+      <div class="container">
+        <div class="section-header text-center">
+          <h2>Chemical Quiz Competition</h2>
+        </div>
+
+		<!--Slider Item-->
+		<div class="sponsor-item">
+          <div class="swiper-container sponsor-slider-one">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img src="images/departments/mechanical/cadmo1.jpg" alt="sponsor image" style="width:100%">
+                  </a>
+                </div>       
+              </div>
+              <div class="swiper-slide">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img src="images/departments/mechanical/cadmo2.jpg" alt="sponsor image" style="width:100%">
+                  </a>
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img src="images/departments/mechanical/cadmo3.jpg" alt="sponsor image" style="width:100%">
+                  </a>
+                </div>    
+              </div>
+              <div class="swiper-slide">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img src="images/departments/mechanical/cadmo4.jpg" alt="sponsor image" style="width:100%">
+                  </a>
+                </div>    
+              </div>
+              <div class="swiper-slide">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img src="images/departments/mechanical/cadmo5.jpg" alt="sponsor image" style="width:100%">
+                  </a>
+                </div>    
+              </div>
+              <div class="swiper-slide">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img src="images/departments/mechanical/cadmo6.png" alt="sponsor image" style="width:100%">
+                  </a>
+                </div>    
+              </div>
+              <div class="swiper-slide">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img src="images/departments/mechanical/cadmo7.jpg" alt="sponsor image" style="width:100%">
+                  </a>
+                </div>    
+              </div>
+            </div>
+          </div>
+        </div><!-- Slider-item -->
+	
+		<!--Content-->
+		<div class="long-content">
+
+			<h4>OVERVIEW </h4>
+
+			<div class="row">
+			    <div class="col-md-4 col-sm-4 col-xs-12">
+				    <div class="thumbnail">
+					    <a href="#">
+					    	<img src="images/sponsors/chevron-logo.jpg" alt="sponsor image" >
+			    		</a>
+			    	</div>  
+			    </div> 
+			</div>  
+
+			<p>
+			Parallel to EXMO 2017, the Premier Engineering and Innovation Exhibition of Sri Lanka, the Department of Chemical 
+      and Process Engineering of University of Moratuwa is organizing a quiz competition for the O/L, A/L students on 
+      both days 28th and 29th of April from 9.00 am onwards at the department premises. 
+
+      This quiz competition will be based on general knowledge, basics of chemical and process engineering, knowledge 
+      acquired from the displayed banners and questions from sponsors.
+
+      At the end of the exhibition all the winners of this competition will be awarded with valuable prizes.
+			</p>
+
+		</div><!--Content-->        
+      
+      </div><!-- container -->
+	</section>
+    <!-- About end here -->
+
+    <?php 
+     include("footer.php");
+    ?>
+
+  </body>
+</html>
